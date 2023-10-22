84b2d0ec0d9696486c3269312bf7602468cab497
Justin Chan <justinchanchan8@gmail.com>
2023-10-23 02:32:03 +0400

    removed extra files

--- main.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import os
-from peft import LoraConfig, get_peft_model, PeftConfig
-from peft import prepare_model_for_kbit_training
-# model_name = "tiiuae/falcon-7b-instruct" 
-import requests
-
-
-if __name__ == "__main__":
